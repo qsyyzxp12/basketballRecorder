b169dfa116594cee9bfe0c592aed9021fad08f95
qsyyzxp12 <b01705040@ntu.edu>
2016-02-23 21:26:10 +0800

    Draw all zone

--- basketBallRecorder/BBRMainViewController.m
+++ basketBallRecorder/BBRMainViewController.m
@@ -11,6 +11,16 @@
 #define TITLE_CELL_HEIGHT 30
 #define CELL_HEIGHT 40
 #define CELL_WIDTH 60
+#define BACKGROUND_WIDTH 373
+#define BACKGROUND_HEIGHT 245
+#define IMAGE_SCALE 0.465
+#define RECORD_LABEL_HEIGHT 23
+#define SIDE_PADDING_RATE 0.25      //for zone 6, 10
+#define TOP_PADDING_RATE1 0.1       //for zone 1, 2, 4, 5, 7, 9, 11
+#define TOP_PADDING_RATE2 0.3       //for zone 3
+#define TOP_PADDING_RATE3 0.6       //for zone 6, 10
+#define TOP_PADDING_RATE4 0         //for zone 8
+
 @interface BBRMainViewController ()
 
 @end
@@ -45,11 +55,10 @@
 - (void) drawPicture
 {
     UIImageView* backgroundImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background.jpg"]];
-    backgroundImageView.frame = CGRectMake(120, 70, self.view.frame.size.height-195, self.view.frame.size.width-75);
-    //backgroundImageView.backgroundColor = [UIColor redColor];
+    backgroundImageView.frame = CGRectMake(120, 70, BACKGROUND_WIDTH, BACKGROUND_HEIGHT);
+    
     backgroundImageView.contentMode = UIViewContentModeScaleAspectFit;
     [self.view addSubview:backgroundImageView];
-    
     
     NSMutableArray* zoneImageViewArray = [NSMutableArray arrayWithCapacity:11];
     
@@ -57,7 +66,7 @@
     UIImageView* zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone1.png"] highlightedImage:[UIImage imageNamed:@"zone1-2.png"]];
     
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(backgroundImageView.frame.origin.x+2, backgroundImageView.frame.origin.y+2, zoneImageView.frame.size.width*0.465, zoneImageView.frame.size.height*0.465);
+    zoneImageView.frame = CGRectMake(backgroundImageView.frame.origin.x+2, backgroundImageView.frame.origin.y+2, zoneImageView.frame.size.width*IMAGE_SCALE, zoneImageView.frame.size.height*IMAGE_SCALE);
 
     zoneImageView.tag = 1;
     
@@ -76,7 +85,7 @@
     CGPoint zonePosition = CGPointMake(zoneImageView.frame.origin.x+zoneImageView.frame.size.width+2, zoneImageView.frame.origin.y);
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone2.png"] highlightedImage:[UIImage imageNamed:@"zone2-2.png"]];
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*0.465, zoneImageView.frame.size.height*0.465);
+    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*IMAGE_SCALE, zoneImageView.frame.size.height*IMAGE_SCALE);
     
     zoneImageView.tag = 2;
     
@@ -95,7 +104,7 @@
     zonePosition = CGPointMake(zoneImageView.frame.origin.x+zoneImageView.frame.size.width+2, zoneImageView.frame.origin.y);
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone3.png"] highlightedImage:[UIImage imageNamed:@"zone3-2.png"]];
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*0.465, zoneImageView.frame.size.height*0.465);
+    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*IMAGE_SCALE, zoneImageView.frame.size.height*IMAGE_SCALE);
     
     zoneImageView.tag = 3;
     
@@ -114,7 +123,7 @@
     zonePosition = CGPointMake(zoneImageView.frame.origin.x+zoneImageView.frame.size.width+2, zoneImageView.frame.origin.y);
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone4.png"] highlightedImage:[UIImage imageNamed:@"zone4-2.png"]];
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*0.465-2, zoneImageView.frame.size.height*0.465);
+    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*IMAGE_SCALE-2, zoneImageView.frame.size.height*IMAGE_SCALE);
     
     zoneImageView.tag = 4;
     
@@ -133,7 +142,7 @@
     zonePosition = CGPointMake(zoneImageView.frame.origin.x+zoneImageView.frame.size.width+2, zoneImageView.frame.origin.y);
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone5.png"] highlightedImage:[UIImage imageNamed:@"zone5-2.png"]];
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*0.465, zoneImageView.frame.size.height*0.465);
+    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*IMAGE_SCALE, zoneImageView.frame.size.height*IMAGE_SCALE);
     
     zoneImageView.tag = 5;
     
@@ -153,7 +162,7 @@
     zonePosition = CGPointMake(zone1.frame.origin.x, zone1.frame.origin.y+zone1.frame.size.height+2);
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone6.png"] highlightedImage:[UIImage imageNamed:@"zone6-2.png"]];
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*0.465, zoneImageView.frame.size.height*0.465-1);
+    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*IMAGE_SCALE, zoneImageView.frame.size.height*IMAGE_SCALE-1);
     
     zoneImageView.tag = 6;
     
@@ -173,7 +182,7 @@
     zonePosition = CGPointMake(zone2.frame.origin.x, zone2.frame.origin.y+zone2.frame.size.height+2);
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone7.png"] highlightedImage:[UIImage imageNamed:@"zone7-2.png"]];
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*0.465, zoneImageView.frame.size.height*0.465);
+    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*IMAGE_SCALE, zoneImageView.frame.size.height*IMAGE_SCALE);
     
     zoneImageView.tag = 7;
     
@@ -193,7 +202,7 @@
     zonePosition = CGPointMake(zone4.frame.origin.x, zone4.frame.origin.y+zone4.frame.size.height+2);
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone10.png"] highlightedImage:[UIImage imageNamed:@"zone10-2.png"]];
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*0.465-2, zoneImageView.frame.size.height*0.465-2);
+    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*IMAGE_SCALE-2, zoneImageView.frame.size.height*IMAGE_SCALE-2);
     
     zoneImageView.tag = 10;
     
@@ -212,7 +221,7 @@
     zonePosition = CGPointMake(zone4.frame.origin.x, zone4.frame.origin.y+zone4.frame.size.height+2);
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone9.png"] highlightedImage:[UIImage imageNamed:@"zone9-2.png"]];
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*0.465-1, zoneImageView.frame.size.height*0.465-1);
+    zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneImageView.frame.size.width*IMAGE_SCALE-1, zoneImageView.frame.size.height*IMAGE_SCALE-1);
     
     zoneImageView.tag = 9;
     
@@ -232,7 +241,7 @@
     UIImageView* zone6 = [self.view viewWithTag:6];
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone11.png"] highlightedImage:[UIImage imageNamed:@"zone11-2.png"]];
     [zoneImageView sizeToFit];
-    CGPoint zoneSize = CGPointMake(zoneImageView.frame.size.width*0.465-2, zoneImageView.frame.size.height*0.465-1);
+    CGPoint zoneSize = CGPointMake(zoneImageView.frame.size.width*IMAGE_SCALE-2, zoneImageView.frame.size.height*IMAGE_SCALE-1);
     zonePosition = CGPointMake(zone3.frame.origin.x+1, CGRectGetMaxY(zone6.frame)-zoneSize.y);
     
     zoneImageView.frame = CGRectMake(zonePosition.x, zonePosition.y, zoneSize.x, zoneSize.y);
@@ -255,7 +264,7 @@
     zonePosition = CGPointMake(zone3.frame.origin.x, zone3.frame.origin.y+zone3.frame.size.height+2);
     zoneImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zone8.png"] highlightedImage:[UIImage imageNamed:@"zone8-2.png"]];
     [zoneImageView sizeToFit];
-    zoneImageView.frame = CGRectMake(zonePosition.x+1, zonePosition.y, zoneImageView.frame.size.width*0.465-2, zoneImageView.frame.size.height*0.465);
+    zoneImageView.frame = CGRectMake(zonePosition.x+1, zonePosition.y, zoneImageView.frame.size.width*IMAGE_SCALE-2, zoneImageView.frame.size.height*IMAGE_SCALE);
     
     zoneImageView.tag = 8;
     
@@ -271,11 +280,11 @@
     [zoneImageViewArray addObject:zoneImageView];
     
     //Draw Label for zone1
-    UILabel* hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone1.frame.origin.x, zone1.frame.origin.y + zone1.frame.size.height*0.1, zone1.frame.size.width, 23)];
+    UILabel* hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone1.frame.origin.x, zone1.frame.origin.y + zone1.frame.size.height*TOP_PADDING_RATE1, zone1.frame.size.width, RECORD_LABEL_HEIGHT)];
     hitRateLabel.textAlignment = NSTextAlignmentCenter;
     hitRateLabel.text = @"0%";
     hitRateLabel.tag = 101;
-    UILabel* gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), CGRectGetWidth(hitRateLabel.frame), 23)];
+    UILabel* gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), CGRectGetWidth(hitRateLabel.frame), RECORD_LABEL_HEIGHT)];
     gradeLabel.textAlignment = NSTextAlignmentCenter;
     gradeLabel.tag = 102;
     gradeLabel.text = @"0/0";
@@ -283,11 +292,11 @@
     [self.view addSubview:gradeLabel];
     
     //Draw Label for zone2
-    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone2.frame.origin.x, zone2.frame.origin.y + zone2.frame.size.height*0.1, zone2.frame.size.width, 23)];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone2.frame.origin.x, zone2.frame.origin.y + zone2.frame.size.height*TOP_PADDING_RATE1, zone2.frame.size.width, RECORD_LABEL_HEIGHT)];
     hitRateLabel.textAlignment = NSTextAlignmentCenter;
     hitRateLabel.text = @"0%";
     hitRateLabel.tag = 201;
-    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), CGRectGetWidth(hitRateLabel.frame), 23)];
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), CGRectGetWidth(hitRateLabel.frame), RECORD_LABEL_HEIGHT)];
     gradeLabel.textAlignment = NSTextAlignmentCenter;
     gradeLabel.tag = 202;
     gradeLabel.text = @"0/0";
@@ -295,11 +304,11 @@
     [self.view addSubview:gradeLabel];
     
     //Draw Label for zone3
-    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone3.frame.origin.x, zone3.frame.origin.y + zone3.frame.size.height*0.1, zone3.frame.size.width, 23)];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone3.frame.origin.x, zone3.frame.origin.y + zone3.frame.size.height*TOP_PADDING_RATE2, zone3.frame.size.width, RECORD_LABEL_HEIGHT)];
     hitRateLabel.textAlignment = NSTextAlignmentCenter;
     hitRateLabel.text = @"0%";
     hitRateLabel.tag = 301;
-    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), CGRectGetWidth(hitRateLabel.frame), 23)];
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), CGRectGetWidth(hitRateLabel.frame), RECORD_LABEL_HEIGHT)];
     gradeLabel.textAlignment = NSTextAlignmentCenter;
     gradeLabel.tag = 302;
     gradeLabel.text = @"0/0";
@@ -307,11 +316,11 @@
     [self.view addSubview:gradeLabel];
     
     //Draw Label for zone4
-    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone4.frame.origin.x, zone4.frame.origin.y + zone4.frame.size.height*0.1, zone4.frame.size.width, 23)];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone4.frame.origin.x, zone4.frame.origin.y + zone4.frame.size.height*TOP_PADDING_RATE1, zone4.frame.size.width, RECORD_LABEL_HEIGHT)];
     hitRateLabel.textAlignment = NSTextAlignmentCenter;
     hitRateLabel.text = @"0%";
     hitRateLabel.tag = 401;
-    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), CGRectGetWidth(hitRateLabel.frame), 23)];
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), CGRectGetWidth(hitRateLabel.frame), RECORD_LABEL_HEIGHT)];
     gradeLabel.textAlignment = NSTextAlignmentCenter;
     gradeLabel.tag = 402;
     gradeLabel.text = @"0/0";
@@ -320,11 +329,11 @@
     
     //Draw Label for zone5
     UIImageView* zone5 = [self.view viewWithTag:5];
-    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone5.frame.origin.x, zone5.frame.origin.y + zone5.frame.size.height*0.1, zone5.frame.size.width, 23)];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone5.frame.origin.x, zone5.frame.origin.y + zone5.frame.size.height*TOP_PADDING_RATE1, zone5.frame.size.width, RECORD_LABEL_HEIGHT)];
     hitRateLabel.textAlignment = NSTextAlignmentCenter;
     hitRateLabel.text = @"0%";
     hitRateLabel.tag = 501;
-    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), zone5.frame.size.width, 23)];
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), zone5.frame.size.width, RECORD_LABEL_HEIGHT)];
     gradeLabel.textAlignment = NSTextAlignmentCenter;
     gradeLabel.tag = 502;
     gradeLabel.text = @"0/0";
@@ -332,13 +341,78 @@
     [self.view addSubview:gradeLabel];
     
     //Draw Label for zone6
-    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone6.frame.origin.x, zone6.frame.origin.y+zone6.frame.size.height*0.6, zone6.frame.size.width/2, 23)];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone6.frame.origin.x, zone6.frame.origin.y+zone6.frame.size.height*TOP_PADDING_RATE3, zone6.frame.size.width*(1-SIDE_PADDING_RATE), RECORD_LABEL_HEIGHT)];
     hitRateLabel.textAlignment = NSTextAlignmentCenter;
     hitRateLabel.text = @"0%";
     hitRateLabel.tag = 601;
-    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), zone6.frame.size.width/2, 23)];
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), zone6.frame.size.width*(1-SIDE_PADDING_RATE), RECORD_LABEL_HEIGHT)];
     gradeLabel.textAlignment = NSTextAlignmentCenter;
     gradeLabel.tag = 602;
+    gradeLabel.text = @"0/0";
+    [self.view addSubview:hitRateLabel];
+    [self.view addSubview:gradeLabel];
+    
+    //Draw Label for zone7
+    UIImageView* zone7 = [self.view viewWithTag:7];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone7.frame.origin.x, zone7.frame.origin.y+zone7.frame.size.height*TOP_PADDING_RATE1, zone7.frame.size.width, RECORD_LABEL_HEIGHT)];
+    hitRateLabel.textAlignment = NSTextAlignmentCenter;
+    hitRateLabel.text = @"0%";
+    hitRateLabel.tag = 701;
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), zone7.frame.size.width, RECORD_LABEL_HEIGHT)];
+    gradeLabel.textAlignment = NSTextAlignmentCenter;
+    gradeLabel.tag = 702;
+    gradeLabel.text = @"0/0";
+    [self.view addSubview:hitRateLabel];
+    [self.view addSubview:gradeLabel];
+    
+    //Draw Label for zone8
+    UIImageView* zone8 = [self.view viewWithTag:8];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone8.frame.origin.x, zone8.frame.origin.y+zone8.frame.size.height*TOP_PADDING_RATE4, zone8.frame.size.width, RECORD_LABEL_HEIGHT)];
+    hitRateLabel.textAlignment = NSTextAlignmentCenter;
+    hitRateLabel.text = @"0%";
+    hitRateLabel.tag = 801;
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), zone8.frame.size.width, RECORD_LABEL_HEIGHT)];
+    gradeLabel.textAlignment = NSTextAlignmentCenter;
+    gradeLabel.tag = 802;
+    gradeLabel.text = @"0/0";
+    [self.view addSubview:hitRateLabel];
+    [self.view addSubview:gradeLabel];
+    
+    //Draw Label for zone9
+    UIImageView* zone9 = [self.view viewWithTag:9];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone9.frame.origin.x, zone9.frame.origin.y+zone9.frame.size.height*TOP_PADDING_RATE1, zone9.frame.size.width, RECORD_LABEL_HEIGHT)];
+    hitRateLabel.textAlignment = NSTextAlignmentCenter;
+    hitRateLabel.text = @"0%";
+    hitRateLabel.tag = 901;
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), zone9.frame.size.width, RECORD_LABEL_HEIGHT)];
+    gradeLabel.textAlignment = NSTextAlignmentCenter;
+    gradeLabel.tag = 902;
+    gradeLabel.text = @"0/0";
+    [self.view addSubview:hitRateLabel];
+    [self.view addSubview:gradeLabel];
+    
+    //Draw Label for zone10
+    UIImageView* zone10 = [self.view viewWithTag:10];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone10.frame.origin.x+zone10.frame.size.width*SIDE_PADDING_RATE, zone10.frame.origin.y+zone10.frame.size.height*TOP_PADDING_RATE3, zone10.frame.size.width*(1-SIDE_PADDING_RATE), RECORD_LABEL_HEIGHT)];
+    hitRateLabel.textAlignment = NSTextAlignmentCenter;
+    hitRateLabel.text = @"0%";
+    hitRateLabel.tag = 1001;
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), zone10.frame.size.width*(1-SIDE_PADDING_RATE), RECORD_LABEL_HEIGHT)];
+    gradeLabel.textAlignment = NSTextAlignmentCenter;
+    gradeLabel.tag = 1002;
+    gradeLabel.text = @"0/0";
+    [self.view addSubview:hitRateLabel];
+    [self.view addSubview:gradeLabel];
+    
+    //Draw Label for zone10
+    UIImageView* zone11 = [self.view viewWithTag:11];
+    hitRateLabel = [[UILabel alloc] initWithFrame:CGRectMake(zone11.frame.origin.x, zone11.frame.origin.y+zone11.frame.size.height*TOP_PADDING_RATE1, zone11.frame.size.width, RECORD_LABEL_HEIGHT)];
+    hitRateLabel.textAlignment = NSTextAlignmentCenter;
+    hitRateLabel.text = @"0%";
+    hitRateLabel.tag = 1101;
+    gradeLabel = [[UILabel alloc] initWithFrame:CGRectMake(hitRateLabel.frame.origin.x, CGRectGetMaxY(hitRateLabel.frame), zone11.frame.size.width, RECORD_LABEL_HEIGHT)];
+    gradeLabel.textAlignment = NSTextAlignmentCenter;
+    gradeLabel.tag = 1102;
     gradeLabel.text = @"0/0";
     [self.view addSubview:hitRateLabel];
     [self.view addSubview:gradeLabel];
